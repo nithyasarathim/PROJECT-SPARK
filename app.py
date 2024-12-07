91fcd823bc0b035d743fda3efea371e9b094ba60
nithyasarathim <nithyasarathi.m2023cse@sece.ac.in>
2024-12-07 10:32:56 +0530

    Project setup

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, send_from_directory
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return send_from_directory('root','index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
